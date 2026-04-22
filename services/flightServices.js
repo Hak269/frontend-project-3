549078d4d65a3c3d3a52ebe31da4c1b88e4117e6
mohammedrashed1 <mhmdmqdad112@gmail.com>
2026-04-22 19:13:26 +0300

    almost finish

--- services/flightServices.js
+++ services/flightServices.js
@@ -4,6 +4,7 @@ const baseUrl = import.meta.env.VITE_BACKEND_URL
 
 async function fetchAllFlights() {
     try {
+        console.log(`${baseUrl}flights`)
         const res = await axios.get(`${baseUrl}/flights`)
         return res.data
     } catch (err) {
