b16592ef7fb348f0d3e9ca24f0ce3cf5c4102878
mohammedrashed1 <mhmdmqdad112@gmail.com>
2026-04-25 15:17:39 +0300

    finish create booking

--- src/App.jsx
+++ src/App.jsx
@@ -29,38 +29,14 @@ function App() {
   return (
     <div>
       <Navbar user={user} setUser={setUser} />
-
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/sign-up" element={!user ? <SignUp /> : <Navigate to='/dashboard'/>} />
         <Route path="/sign-in" element={!user ? <SignIn setUser={setUser} /> : <Navigate to='/dashboard'/>} />
         <Route path="/dashboard" element={user ? <Dashboard user={user} /> : <Navigate to='/sign-in'/>} />
         <Route path='/flights' element={<AllFlights/>} />
-
-        <Route
-          path="/sign-up"
-          element={!user ? <SignUp /> : <Navigate to="/dashboard" />}
-        />
-
-        <Route
-          path="/sign-in"
-          element={!user ? <SignIn setUser={setUser} /> : <Navigate to="/dashboard" />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={user ? <Dashboard user={user} /> : <Navigate to="/sign-in" />}
-        />
-
-        <Route
-          path="/booking"
-          element={<BookingDetails user={user} />}
-        />
-
-        <Route
-          path="/filteredFlights"
-          element={<SearchFlights user={user} />}
-        />
+        <Route path="/booking" element={<BookingDetails user={user} />} />
+        <Route path="/filteredFlights" element={<SearchFlights user={user} />} />
       </Routes>
     </div>
   );
